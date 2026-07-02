ed3ab1444c894c25a8a556bf463b032a383014fd
Rohit singh <singhrohit77443@gmail.com>
2026-07-02 16:20:13 +0530

    rank&theme

--- src/stores/theme/index.tsx
+++ src/stores/theme/index.tsx
@@ -60,7 +60,7 @@ const isHalloween =
 export const useThemeStore = create(
   persist(
     immer<ThemeStore>((set) => ({
-      theme: is420 ? "green" : isHalloween ? "autumn" : null,
+      theme: is420 ? "green" : isHalloween ? "autumn" : "green",
       customTheme: {
         primary: "classic",
         secondary: "classic",
